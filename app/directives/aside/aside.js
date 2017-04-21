a6a4d63285021692111ede59fdd55da771d5fb64
feige <626138959@qq.com>
2017-04-21 23:55:24 +0800

    aside自定义指令

--- app/directives/aside/aside.js
+++ app/directives/aside/aside.js
@@ -5,10 +5,19 @@ mod.directive('bxgAside', [function(){
 		restrict: 'EM',
 		replace: true,
 		scope: {
-			
+			focus: '@'
 		},
 		link: function(scope, ele, attr) {
 			
-		}
+		},
+		controller: [
+            '$scope',
+            function ($scope) {
+
+				// 左侧导航课程管理的显示隐藏
+                $scope.isCourseManager = /^course/.test($scope.focus)? true: false;
+
+            }
+        ]
 	};
 }]);

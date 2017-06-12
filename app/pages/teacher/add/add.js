efc75eeaea002bde9196fbf5d6a8df498222add7
PengfeiGuo <626138959@qq.com>
2017-06-13 00:13:10 +0800

    讲师表单效验bug修复

--- app/pages/teacher/add/add.js
+++ app/pages/teacher/add/add.js
@@ -22,7 +22,7 @@ angular
 			$scope.teacherAdd = function() {
 				
 				// 表单效验通过后再提交，这里效验是因为用户可能通过回车等方式提交表单，而非点击提交按钮
-				if($scope.teacherAddForm.$invalid) {
+				if($scope.teacherAddForm.$valid) {
 					
 					// 传入form元素，ajax服务会自动获取form中的值
 					// 备注：这里没有使用scope数据是为了测试其他可行方式
--- app/pages/teacher/edit/edit.js
+++ app/pages/teacher/edit/edit.js
@@ -27,7 +27,7 @@ angular
 			$scope.modify = function() {
 				
 				// 表单效验通过后再提交，这里效验是因为用户可能通过回车等方式提交表单，而非点击提交按钮
-				if($scope.teacherEditForm.$invalid) {
+				if($scope.teacherEditForm.$valid) {
 					
 					ajaxService.teacherModify($scope.teacher, function(result) {
 						$location.path('/teacher/list');
